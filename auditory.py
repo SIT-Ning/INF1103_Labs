ba8eced136652eed7c2f8528167748db397d0d26
SIT-Ning <2605753@sit.singaporetech.edu.sg>
2026-09-11 11:03:27 +0800

    Second Lab

---
+++ auditory.py
@@ -0,0 +1,15 @@
+inventory = 0
+rejected = 0
+while True:
+    stock = input("Enter a stock: ")
+    if stock == "quit":
+        break
+    elif stock.isdigit() == False:
+        print("Error")
+        rejected += 1
+    elif inventory > 500:
+        break
+    else:
+        inventory += int(stock)
+print("Total Unit Processed:", inventory, "No. of rejected entries:", rejected)
+    
