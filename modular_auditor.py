b2a26899ce153a4c97fd80542587a2e0ead2ffce
SIT-Ning <2605753@sit.singaporetech.edu.sg>
2026-09-18 12:19:14 +0800

    Third save

--- modular_auditor.py
+++ modular_auditor.py
@@ -5,12 +5,13 @@ def get_valid_input():
     elif stock.isdigit() == False:
         return "Invalid No."
     else:
-        return stock
+        return int(stock)
 def process_delivery(current_total, new_value):
     new_total = current_total + new_value
     return new_total
 def calculate_tax(amount):
-    amount_tax = amount * 0.1
+    tax = 0.1
+    amount_tax = amount * tax 
     return amount_tax
 def generate_report(total_units, failed_attempts):
     print("Total Unit Processed:", total_units ,"|", "No. of rejected entries:", failed_attempts)
@@ -26,5 +27,13 @@ while True:
     elif entry == "Invalid No.":
         print("Error")
         rejected += 1
-    elif 
+    elif inventory + entry > 500:
+        print("Units exceeded 500 units")
+        break
+    else:
+        inventory = process_delivery(inventory, entry)
+        calculate_tax(entry)
+
+generate_report(inventory, rejected)
+
     
